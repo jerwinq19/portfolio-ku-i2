0ff1e0fb7055f026a40d9f49b147b8d7094e3ff3
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-02-16 00:13:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <h1>hello world</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, veniam dolor quae reiciendis, totam dignissimos consectetur et harum magnam nisi doloremque nemo illo deleniti dicta explicabo modi praesentium quis voluptatem!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi magnam, porro rem quasi dolorem dolores at delectus nulla eum libero placeat laudantium culpa aperiam accusamus ut omnis numquam! Repudiandae, temporibus.</p>
     </>
   );
 }
