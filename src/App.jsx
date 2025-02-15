0c3451f3a600de8627f70733f811d14b6e272868
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-02-16 00:08:47 +0800

    deployed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const count = useState(0)
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, veniam dolor quae reiciendis, totam dignissimos consectetur et harum magnam nisi doloremque nemo illo deleniti dicta explicabo modi praesentium quis voluptatem!</p>
+    </>
+  );
+}
+
+export default App;
